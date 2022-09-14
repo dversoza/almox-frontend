1c7c472aaa1fe0c68ca5ca2bef9535caa7a02dee
dversoza <daniel.ve3@gmail.com>
2022-09-13 23:27:17 -0300

    Creates year navigation functionalities

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { LoginService } from 'src/app/auth/services/login.service';
 import { User } from 'src/app/shared';
 
+const TRANSACTION_YEAR_STORAGE_KEY = 'transactionYear';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -20,5 +22,21 @@ export class NavbarComponent implements OnInit {
   logout() {
     this.loginService.logout();
     this.router.navigate(['/login']);
+  }
+
+  public resetToCurrentYear(): void {
+    localStorage.removeItem(TRANSACTION_YEAR_STORAGE_KEY);
+    this.router.navigate(['/']);
+  }
+
+  public hasTransactionYearSelected(): boolean {
+    return !!localStorage.getItem(TRANSACTION_YEAR_STORAGE_KEY);
+  }
+
+  get getTransactionYear(): string {
+    if (this.hasTransactionYearSelected()) {
+      return localStorage.getItem(TRANSACTION_YEAR_STORAGE_KEY) as string;
+    }
+    return '';
   }
 }
