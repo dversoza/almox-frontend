d744f4160ce40e0c5d249df2e49c7c33812e88d1
dversoza <daniel.ve3@gmail.com>
2023-01-18 19:51:48 -0300

    feat: add rate-limiter to application

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //Install express server
 const express = require("express");
+const rateLimit = require('express-rate-limit')
+
 const path = require("path");
 
 const app = express();
@@ -11,5 +13,15 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/almox/index.html"));
 });
 
-// Start the app by listening on the default Heroku port
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+
+// Start the app by listening on the default port
 app.listen(process.env.PORT || 8080);
